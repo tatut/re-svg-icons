bc236886fc81b186888226bb161dcd279db2bee0
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2020-03-24 14:37:19 +0200

    Add script to download sources (for generation purposes only)

---
+++ icon-sources/clone-icons.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git clone git@github.com:feathericons/feather.git
+git clone git@github.com:tabler/tabler-icons.git
+git clone git@github.com:iconic/open-iconic.git
+git clone git@github.com:refactoringui/heroicons.git
